2da8c457434b84f8a78b7a8a5032cf0f9e138dfb
Dmitry <kochetov.dmitry@mail.ru>
2023-10-28 16:45:30 +0300

    добавлена ветка деплоя

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publickPath: `/Photogallery/`,
+  publicPath: `/Photogallery/`,
   transpileDependencies: true
 })
